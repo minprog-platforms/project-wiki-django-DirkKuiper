15d9116000581bf3edb23834449c4bb41cc52d57
DirkKuiper <dirk@kvdm.nl>
2021-12-01 14:43:38 +0100

    added entry page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,3 +1,4 @@
+import markdown2
 from django.shortcuts import render
 
 from . import util
@@ -6,5 +7,12 @@ from . import util
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+    })
+
+def entry(request, page):
+    return render(request, "encyclopedia/entry.html", {
+        # Converts markdown to html
+        "entry": markdown2.markdown(util.get_entry(page)),
+        "title": page
     })
 
